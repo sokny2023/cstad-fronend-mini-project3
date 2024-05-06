9a07d1ba8792b9b84f044282e8ad764a9ddc4491
sokny2023 <psnpsn667788@gmail.com>
2024-05-06 23:14:08 +0700

    update navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow:{
+        'ss1':"rgba(0, 0, 0, 0.1) 0px 20px 20px -20px"
+      }
     },
   },
   plugins: [require("flowbite/plugin")],
